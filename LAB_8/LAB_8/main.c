e266227472f26e72138272df6c15a509bf23d86f
Brent Son <brentson00@gmail.com>
2022-12-01 17:24:05 -0800

    first commit

---
+++ LAB_8/LAB_8/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB_8.c
+ *
+ * Created: 12/1/2022 5:20:27 PM
+ * Author : Brent Son
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
